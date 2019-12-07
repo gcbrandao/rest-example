b8e6f8b9d6e22f1f67992f02987e40c956fc9536
Gil Brandao <gcbrandao@gmail.com>
2019-12-07 11:56:37 -0300

    alterações finais nas classes e pom

--- src/test/java/com/example/restexemple/RestExempleApplicationTests.java
+++ src/test/java/com/example/restexemple/RestExempleApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.restexemple;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RestExempleApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.restexemple;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class RestExempleApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
